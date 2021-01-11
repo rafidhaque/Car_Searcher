8b4d9490fa831835d7e666bc97399c39618a3dd0
rafidhaque <rafidhaque@iut-dhaka.edu>
2021-01-11 16:34:09 +0600

    controlled vs uncontrolled component

--- car_searcher/src/components/SearchBar.js
+++ car_searcher/src/components/SearchBar.js
@@ -1,21 +1,22 @@
 import React from "react";
 
 class SearchBar extends React.Component {
-  onInputChange(event) {
-    console.log(event.target.value);
-  }
+  state = {
+    term: "",
+    warning: "must be 4 letter",
+  };
 
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
   render() {
     return (
       <div className="ui segment">
         <form className="ui form">
           <div className="field">
-            <label>Image Search</label>
-            <input type="text" onChange={this.onInputChange}></input>
+            <label>{this.state.term.length < 4 ? "must be 4" : ""}</label>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={(e) => this.setState({ term: e.target.value })}
+            ></input>
           </div>
         </form>
       </div>
