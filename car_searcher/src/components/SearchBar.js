fee7815accce3896a60bbc621c004d82261549d8
rafidhaque <rafidhaque@iut-dhaka.edu>
2021-01-11 16:37:07 +0600

    stop auto submit

--- car_searcher/src/components/SearchBar.js
+++ car_searcher/src/components/SearchBar.js
@@ -6,17 +6,25 @@ class SearchBar extends React.Component {
     warning: "must be 4 letter",
   };
 
+  onFormSubmit(event) {
+    event.preventDefault();
+  }
+
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
-            <label>{this.state.term.length < 4 ? "must be 4" : ""}</label>
             <input
               type="text"
               value={this.state.term}
               onChange={(e) => this.setState({ term: e.target.value })}
             ></input>
+            <label>
+              {this.state.term.length < 4 && this.state.term.length > 0
+                ? "Type at least 4 characters"
+                : ""}
+            </label>
           </div>
         </form>
       </div>
